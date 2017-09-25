823dd80ec9f0b67c205e877a90bfc03080cefdf4
Samuel Quintana <sock.sqt@gmail.com>
2017-09-24 20:26:35 -0500

    First version of the controller, tested with relay

---
+++ README.md
@@ -0,0 +1,6 @@
+Controll for simple device
+
+# Git dependencies
+
+git clone https://github.com/avishorp/TM1637.git
+
---
+++ waterbomb-ctrl.ino
@@ -0,0 +1,92 @@
+#include <EEPROM.h>
+#include <TM1637Display.h>
+
+#define BUTTON_PIN        2  // Button
+#define RELAY_OUT_PIN     3  // RELAY
+#define CLK               4
+#define DIO               5
+#define DELAY            20  // Delay per loop in ms
+
+const unsigned int ONE_SECOND = 1000; // 1 second
+const unsigned int TEN_SECONDS = 10 * ONE_SECOND; // 1 minute
+const unsigned int ONE_MINUTE = 60 * ONE_SECOND; // 1 minute
+
+int counter_addr = 0;
+byte minutes_left = 0;
+
+// Variables
+boolean button_was_pressed; // previous state
+
+// time controls
+unsigned long init_time;
+
+TM1637Display display(CLK, DIO);
+
+boolean handle_button() {
+  boolean event;
+  int button_now_pressed = !digitalRead(BUTTON_PIN); // pin low -> pressed
+
+  event = button_now_pressed && !button_was_pressed;
+  button_was_pressed = button_now_pressed;
+  return event;
+}
+
+void print_to_display(int number) {
+  display.showNumberDec(number);
+}
+
+void setup() {
+  Serial.begin(9600);
+  display.setBrightness(0x0f);
+
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(RELAY_OUT_PIN, OUTPUT);
+
+  button_was_pressed = false;
+
+  minutes_left = EEPROM.read(counter_addr);
+  print_to_display(minutes_left);
+
+  Serial.println(minutes_left);
+  
+  if (minutes_left > 0) {
+    digitalWrite(RELAY_OUT_PIN, HIGH);
+  }
+  digitalWrite(BUTTON_PIN, HIGH);
+}
+
+void loop() {
+  // handle button
+  boolean raising_edge = handle_button();
+
+  if (raising_edge) {
+    Serial.print("\n## Pushed button");
+    Serial.print("\n After: ");
+    Serial.print(minutes_left);
+    minutes_left += 0x05;
+    Serial.print(", Before: ");
+    Serial.print(minutes_left);
+    EEPROM.write(counter_addr, minutes_left);
+
+    print_to_display(minutes_left);
+    digitalWrite(RELAY_OUT_PIN, HIGH);
+    init_time = millis();
+  }
+  if (minutes_left == 0x00 && digitalRead(RELAY_OUT_PIN)) {
+    digitalWrite(RELAY_OUT_PIN, LOW);
+  }
+
+  if ((millis() - init_time) >= ONE_MINUTE) {
+    if (minutes_left > 0x00) {
+      minutes_left--;
+      init_time = millis();
+
+      EEPROM.write(counter_addr, minutes_left);
+
+      print_to_display(minutes_left);
+    }
+  }
+
+  delay(DELAY);
+}
+

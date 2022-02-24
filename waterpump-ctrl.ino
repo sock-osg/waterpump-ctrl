242ab3383674377b87e1f4268eb2978b21d447c1
Samuel Quintana <sock.sqt@gmail.com>
2022-02-23 23:00:13 -0600

    bugfix: Allows reconnect to wifi and mqtt. Added led indicators

--- waterpump-ctrl.ino
+++ waterpump-ctrl.ino
@@ -11,12 +11,18 @@ const char* STATUS_TOPIC = "STATUS_TOPIC";
 const char* TIMMER_TOPIC = "TIMMER_TOPIC";
 const char* DEVICE_ID = "DEVICE_ID";
 
-#define BUTTON_PIN        13  // D7 - Button
-#define RELAY_OUT_PIN     12  // D6 - To relay
 #define CLK               4   // D2
 #define DIO               5   // D1
+#define RELAY_OUT_PIN     12  // D6
+#define BUTTON_PIN        13  // D7
+#define WIFI_INDICATOR    14  // D5
+#define MQTT_INDICATOR    15  // D8
 
 #define DELAY            20  // Delay per loop in ms
+
+bool wifi_connected = false;
+bool mqtt_connected = false;
+bool wifi_connecting = false;
 
 const unsigned int ONE_SECOND = 1000; // 1 second
 const unsigned int TEN_SECONDS = 10 * ONE_SECOND; // 10 seconds
@@ -37,7 +43,11 @@ PubSubClient mqtt_client(espClient);
 void print_save_and_publish(int number) {
   EEPROM.write(counter_addr, number);
   EEPROM.commit();
-  mqtt_client.publish(STATUS_TOPIC, String(number).c_str());
+
+  if (mqtt_connected) {
+    mqtt_client.publish(STATUS_TOPIC, String(number).c_str());
+  }
+
   display.showNumberDec(number);
 }
 
@@ -54,24 +64,55 @@ void callback(char* topic, byte* payload, unsigned int length) {
   print_save_and_publish(minutes_left);
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!mqtt_client.connected()) {
-    Serial.print("Attempting MQTT connection...");
+void wifi_connect() {
+  if (!wifi_connecting) {
+    wifi_connecting = true;
+    Serial.print(" ===> Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    Serial.print(" -> .");
+  } else {
+    Serial.print(".");
+  }
+
+  delay(500);
+
+  wifi_connected = WiFi.status() == WL_CONNECTED;
+  if (wifi_connected) {
+    wifi_connecting = false;
+
+    Serial.println("");
+    Serial.print(" -> WiFi connected\n    + IP assigned: ");
+    Serial.print(WiFi.localIP());
+    Serial.println("");
+  }
+
+  digitalWrite(WIFI_INDICATOR, wifi_connected);
+  digitalWrite(MQTT_INDICATOR, mqtt_connected);
+}
+
+void mqtt_connect() {
+  if (mqtt_connected) {
+    digitalWrite(MQTT_INDICATOR, mqtt_connected);
+    return;
+  } else {
+    Serial.println(" -> Attempting MQTT connection");
     // Attempt to connect
     // If you do not want to use a username and password, change next line to
     if (mqtt_client.connect(DEVICE_ID)) {
     // if (mqtt_client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
-      Serial.println("connected");
+      Serial.println("    + connected");
       mqtt_client.subscribe(TIMMER_TOPIC);
+      mqtt_connected = true;
+      print_save_and_publish(minutes_left);
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqtt_client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      Serial.print("    + failed, rc=");
+      Serial.println(mqtt_client.state());
+      mqtt_connected = false;
     }
   }
+
+  digitalWrite(MQTT_INDICATOR, mqtt_connected);
 }
 
 void setup() {
@@ -82,6 +123,8 @@ void setup() {
 
   btn_control.init();
   pinMode(RELAY_OUT_PIN, OUTPUT);
+  pinMode(WIFI_INDICATOR, OUTPUT);
+  pinMode(MQTT_INDICATOR, OUTPUT);
 
   minutes_left = EEPROM.read(counter_addr);
   display.showNumberDec(minutes_left);
@@ -90,32 +133,21 @@ void setup() {
     digitalWrite(RELAY_OUT_PIN, HIGH);
   }
 
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
- 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
-  // Print the IP address
-  Serial.print("IP assigned: ");
-  Serial.print(WiFi.localIP());
+  wifi_connect();
 
   mqtt_client.setServer(mqtt_server, 1883);
   mqtt_client.setCallback(callback);
 }
 
 void loop() {
-  if (!mqtt_client.connected()) {
-    reconnect();
+  wifi_connected = WiFi.status() == WL_CONNECTED;
+  if (!wifi_connected) {
+    wifi_connect();
+  }
+
+  mqtt_connected = mqtt_client.connected();
+  if (wifi_connected && !mqtt_connected) {
+    mqtt_connect();
   }
 
   int event = btn_control.handle();
